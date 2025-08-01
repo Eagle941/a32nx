f19be9b13b6ff180567ce4ff6993417dc6e50951
eagle941 <8973725+Eagle941@users.noreply.github.com>
2025-08-01 01:21:47 +0100

    feat(arinc): fixed parity bit calculation

--- fbw-common/src/wasm/systems/systems/src/shared/arinc429_better.rs
+++ fbw-common/src/wasm/systems/systems/src/shared/arinc429_better.rs
@@ -14,15 +14,16 @@ impl Arinc429WordBetter {
         let data_ext = value.into();
         let data = data_ext & ((0b1 << 19) - 1);
         debug_assert_eq!(data_ext, data); // `data` must fit in 19bits
-        let p = Self::calculate_parity_bit(data);
 
-        Self {
+        let mut word = Self {
             label: 0,
             sdi: 0,
             data,
             ssm,
-            p,
-        }
+            p: 0.into(),
+        };
+        word.p = Self::calculate_parity_bit(word.into());
+        word
     }
 
     pub fn new_with_label<T: Into<u32>>(value: T, ssm: SignStatus, sdi: u8, label: u8) -> Self {
@@ -33,25 +34,27 @@ impl Arinc429WordBetter {
         let data_ext = value.into();
         let data = data_ext & ((0b1 << 19) - 1);
         debug_assert_eq!(data_ext, data); // `data` must fit in 19bits
-        let p = Self::calculate_parity_bit(data);
 
-        Self {
+        let mut word = Self {
             label,
             sdi,
             data,
             ssm,
-            p,
-        }
+            p: 0.into(),
+        };
+        word.p = Self::calculate_parity_bit(word.into());
+        word
     }
 
     fn calculate_parity_bit(value: u32) -> Parity {
-        let mut value = value;
-        let mut parity = 0;
-        while value > 0 {
-            let extracted_value = value % 2;
-            value >>= 1;
-            parity = parity ^ extracted_value;
-        }
+        // Taken from https://graphics.stanford.edu/~seander/bithacks.html#ParityParallel
+        let mut v = value;
+        v ^= v >> 16;
+        v ^= v >> 8;
+        v ^= v >> 4;
+        v &= 0xf;
+        let parity: u32 = (0x6996 >> v) & 1;
+        let parity: u32 = (parity + 1) % 2;
         return parity.into();
     }
 
@@ -97,13 +100,14 @@ impl Arinc429WordBetter {
     }
 
     pub fn is_correct_parity(&self) -> bool {
-        self.p == Self::calculate_parity_bit(self.data)
+        self.p == Self::calculate_parity_bit(Into::<u32>::into(*self))
     }
 
     pub fn set_bit(&mut self, bit: u8, value: bool) {
         debug_assert!((11..=29).contains(&bit));
         self.data = ((self.data) & !(1 << (bit - 1))) | ((value as u32) << (bit - 1));
-        self.p = Self::calculate_parity_bit(self.data);
+        self.p = 0.into();
+        self.p = Self::calculate_parity_bit(Into::<u32>::into(*self));
     }
 
     pub fn get_bit(&self, bit: u8) -> bool {
@@ -111,12 +115,6 @@ impl Arinc429WordBetter {
         ((self.data >> (bit - 1)) & 1) != 0
     }
 }
-
-// let label = 0b10110000;
-// let sdi = 0b00;
-// let data = 0b1000110001100010001;
-// let ssm = 0b00;
-// let p = 0b1;
 
 // All LVARs are 64bit. A429 signals need to be converted to f64 when written or read from
 // the variables registry.
@@ -141,14 +139,20 @@ impl From<f64> for Arinc429WordBetter {
         word
     }
 }
+impl From<Arinc429WordBetter> for u32 {
+    fn from(value: Arinc429WordBetter) -> u32 {
+        let label: u32 = value.label.into();
+        let ssm: u32 = value.ssm.into();
+        let sdi: u32 = value.sdi.into();
+        let data: u32 = value.data.into();
+        let parity: u32 = value.p.into();
+        let int_value: u32 = label | (sdi << 8) | (data << 10) | (ssm << 29) | (parity << 31);
+        int_value
+    }
+}
 impl From<Arinc429WordBetter> for f64 {
     fn from(value: Arinc429WordBetter) -> f64 {
-        let label: u64 = value.label.into();
-        let ssm: u64 = value.ssm.into();
-        let sdi: u64 = value.sdi.into();
-        let data: u64 = value.data.into();
-        let parity: u64 = value.p.into();
-        let int_value: u64 = label | (sdi << 8) | (data << 10) | (ssm << 29) | (parity << 31);
+        let int_value: u32 = value.into();
 
         int_value as f64
     }
@@ -162,7 +166,7 @@ pub enum SignStatus {
     NormalOperation,
 }
 
-impl From<SignStatus> for u64 {
+impl From<SignStatus> for u32 {
     fn from(value: SignStatus) -> Self {
         match value {
             SignStatus::FailureWarning => 0b00,
@@ -191,7 +195,7 @@ pub enum Parity {
     Even,
 }
 
-impl From<Parity> for u64 {
+impl From<Parity> for u32 {
     fn from(value: Parity) -> Self {
         match value {
             Parity::Odd => 0b00,
@@ -213,6 +217,11 @@ impl From<u32> for Parity {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn parity_count_ones(data: u32) -> Parity {
+        let number_ones = data.count_ones() + 1;
+        (number_ones % 2).into()
+    }
 
     #[test]
     fn test_simple_word() {
@@ -242,6 +251,39 @@ mod tests {
         assert_eq!(result.sdi, sdi);
         assert_eq!(result.label, label);
         assert_eq!(result.p, expected_parity);
+    }
+
+    #[test]
+    fn test_calculate_parity_bit() {
+        let data = 0b10001111111000100010010110000;
+        assert_eq!(
+            Arinc429WordBetter::calculate_parity_bit(data),
+            parity_count_ones(data)
+        );
+
+        let data = 0b0;
+        assert_eq!(
+            Arinc429WordBetter::calculate_parity_bit(data),
+            parity_count_ones(data)
+        );
+
+        let data = 0b1;
+        assert_eq!(
+            Arinc429WordBetter::calculate_parity_bit(data),
+            parity_count_ones(data)
+        );
+
+        let data = 0b01111111111111111111111111111111;
+        assert_eq!(
+            Arinc429WordBetter::calculate_parity_bit(data),
+            parity_count_ones(data)
+        );
+
+        let data = 0b01111111111111111111111111111110;
+        assert_eq!(
+            Arinc429WordBetter::calculate_parity_bit(data),
+            parity_count_ones(data)
+        );
     }
 
     #[ignore = "This test proves that no data is lost casting from u32 to f64"]
